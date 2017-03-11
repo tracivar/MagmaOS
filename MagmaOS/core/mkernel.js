347d63036ed95e3a1b90a810454ee55f2a3512a8
Matthew Hall <Matthew.Hall@greymethod.org.uk>
2017-03-11 16:07:23 +0000

    Constructed initial magma namespace and web worker based module loading system. Work still required to facilitate two way communication between worker nodes.

--- MagmaOS/core/mkernel.js
+++ MagmaOS/core/mkernel.js
@@ -1,1 +1,82 @@
 'use strict'
+
+//Module parameters. Change this to reflect what your module does.
+var $module = new Object();
+
+$module.Name = "Kernel";
+$module.Description = "Handles low level operations and interactions with external services.";
+
+
+//This is a magma primining module. There is some initialization for communication and management. Do not modify this code.
+var $magma = new Object();
+(function () {
+    this.Status = { Undefined: 0, Success: 1, Error: 2 };
+    //Standard model for communicating messages to the shell.
+    this.Message = function (name,message,status) {
+            this.StatusCode = $magma.Status.Undefined;
+            this.Name = "";
+            this.SenderIdentity = null;
+            this.VerboseMessage = "";
+            this.Payload = {};
+    }
+    //Standard Model for communicating scheduler commands to the shell.
+    this.SchedulerCommand = function (command) {
+        this.Command = command;
+        this.Args = {};
+        this.Name = "_sys";
+        this.Reason = "";
+    }
+    this.Init = function () {
+        $module.Init();
+    }
+    //The magma module intially handles the request coming in and splits it out into an event-driven model.
+    this.MessageProcessor = function (ev) {
+        console.log(ev);
+    }
+    self.onmessage += this.MessageProcessor;
+    //Usage: Create a message or command object using the types above ($magma.Message) and use self.postMessage to send it to the surface.
+
+    /*  
+    
+        Creating functionality: Your application should implement event listeners for recieving commands from the surface, as 
+        well as a heartbeat to keep the scheduler from terminating and deleting your module at runtime.
+
+        The worker scope has no access to the DOM or variables declared in the global scope. This keeps your application sandboxed from
+        the rest of the environment. You must communicate with it using messages, including any data which you wish to surface to
+        applications.
+
+        Surface applications are not run in user-space. Whilst third-parties can develop functionality for magma, only functionality
+        developed expressly by the team managing the project must exist in the kernel-space. For this reason, modification of the
+        two main components, kernel.js and boot.js is strictly forbidden. 
+
+        To use presentation features such as forms and graphics you must interact by posting messages containing payloads that consist of
+        compatible objects for the latest version of the presentation layer/shell. The shell runtime is a seperate piece of functionality
+        and oversees the drawing of the interface and proxies interaction between modules.
+
+        Events and actions in any GUI result in the creation of event objects which are passed onto the relevant module. Failure to respond
+        to a UI request in sufficient time will essentially result in your application 'not responding' and facing subsequent termination,
+        which as mentioned above includes termination of your module.
+
+        */
+
+}).bind($magma)();
+
+//The heartbeat event is called every 10 seconds and tells the shell that we are not stuck in a loop.
+$module.Heartbeat = function (_) {
+    var message = new $magma.SchedulerCommand("alive");
+    self.postMessage(message);
+}
+
+$module.Init = function (_) {
+    var message = { "StatusCode": 0, "Name": "Kernel", "VerboseMessage": "Injection succesful.", "Payload": {} };
+    self.postMessage(message);
+}
+
+$module.Exit = function (_) {
+
+}
+
+
+
+//Don't change this, make sure this executes last.
+$magma.Init();
--- MagmaOS/runtime/boot.js
+++ MagmaOS/runtime/boot.js
@@ -1,11 +1,36 @@
 //The bootloader is responsive for initializing the $magma namespace and facilitating communication between the kernel and other modules.
 (function () {
-    if ($magma) {
+    if (typeof ($magma) !== "undefined") {
         console.log("Magma already loaded. Error during initialization.");
     } else {
-        $magma = function () {
-            this.Version = { Major: 0, Minor: 1, Revision: 1, Name: "Mainline" };
-        }();
+
+        $magma = new (function () {
+            var _version = { Major: 0, Minor: 1, Revision: 1, Name: "Mainline" };
+            var _messages = 42;
+            var _initFunc = function () {
+                //This is an important function that is the catalyst for runtime.
+                //Change carefully!
+                _messages = new $magma.MessageBus();
+
+                //Test the message bus.
+                aMessage = new $magma.Message();
+                aMessage.SenderIdentity = "BootLoader";
+                aMessage.VerboseMessage = "The bootloader has initialised the message bus and will begin loading the kernel...";
+                _messages.Tell(aMessage);
+
+                //Add the kernel module.
+                var kernel = new $magma.ModuleDefinition("Kernel", "/core/mkernel.js", $magma.ModuleDefinitionType.Kernel);
+                $magma.Modules.AddModule = kernel;
+
+                //Load all modules.
+                $magma.Modules.LoadAll();
+            }
+            Object.defineProperty(this, "Version", { get: function () { return _version; } });
+            Object.defineProperty(this, "Init", { get: function () { return _initFunc; } });
+            this.Tell = function (a) {
+                    _messages.Tell(a);
+            };
+        })();
 
         //Low level exception handling.
         $magma.Exception = function (message) {
@@ -13,47 +38,213 @@
             this.name = "MagmaRuntimeException";
         }
 
-
-        $magma.Modules = {
-            _modules: new Array(),
-            set AddModule(module) {
-                if (module instanceof $magma.ModuleDefinition) {
-                    //Duplicates not allowed.
-                    for (var n = 0; n < _modules.length; n++) {
-                        if (_modules[n])
-                    }
+        $magma.MessageBus = function () {
+            var _messages = new Array();
+            this.Tell = function (message) {
+                if (typeof(message.VerboseMessage) !== "undefined") {
+                    console.log("[" + (new Date().toLocaleTimeString()) + "] [%c"+message.SenderIdentity+"%c] %c" + message.VerboseMessage, "font-weight: bold; color: #f00; letter-spacing:5px;","","font-weight: bold; color: #135;");
                 } else {
-                    throw new $magma.Exception("The provided module was not of the correct type.");
+                    throw new $magma.Exception("Only valid magma message objects are permitted through transmission to the Tell method.");
                 }
             }
         }
 
+        //This object contains the necessary properties for interacting with a module. It gets passed directly to the worker object.
+        $magma.MethodRequest = function () {
+            var _completed = 0;
+            var _sent = 0;
+            var _error = 0;
+            var _targetModule = "";
+            var _request = "";
+            var _args = {};
+            var _response = {};
+            //Indicates whether a response was recieved from the module.
+            Object.defineProperty(this, "Completed", { get: function () { return _completed; } });
+            //Indicates whether the request has been sent to the module.
+            Object.defineProperty(this, "Sent", { get: function () { return _sent; } });
+            //Indicates whether an error ocurred trying to send the request the module.
+            Object.defineProperty(this, "Error", { get: function () { return _sent; } });
+        }
+
+        //The module system handles the book-keeping and loading of components.
+        $magma.ModuleManager = function () {
+            var _modules = new Array();
+            Object.defineProperty(this, "AddModule", {
+                set: function (module) {
+                    if (module instanceof $magma.ModuleDefinition) {
+
+                        //Duplicates not allowed.
+                        for (var n = 0; n < _modules.length; n++) {
+                            if (_modules[n].Name.toLowerCase() == module.Name.toLowerCase()) {
+                                throw new $magma.Exception("The module with name '" + module.Name + "' already exists in the chain.");
+                                break;
+                            }
+                        }
+                        _modules[_modules.length] = module;
+                    } else {
+                        throw new $magma.Exception("The provided module was not of the correct type.");
+                    }
+                }
+            });
+            //This should not be enabled in production
+            Object.defineProperty(this, "Modules", { get: function () { return _modules; } });
+            Object.defineProperty(this, "Select", {
+                set: function (name) {
+                    for (var n = 0; n < _modules.length; n++) {
+                        if (_modules[n].Name.toLowerCase() == name) {
+                            return _modules[n];
+                        }
+                    }
+                }
+            });
+            this.toString = function () {
+                return "Magma Module System";
+            }
+            this.LoadAll = function () {
+                for (var n = 0; n < _modules.length; n++) {
+                    if (_modules[n].State == $magma.ModuleDefinitionState.Unloaded) {
+                        _modules[n].Load();
+                    }
+                }
+            }
+        }
+
+        Object.freeze($magma.ModuleManager);
+
+        $magma.Modules = new $magma.ModuleManager();
+
         $magma.ModuleDefinitionType = { Undefined: 0, Kernel: 1, Shell: 2, Application: 3, Peripheral: 4 };
-        $magma.Status = { Undefined: 0, Success: 1, Error: 2};
+        $magma.Status = { Undefined: 0, Success: 1, Error: 2 };
+        $magma.ModuleDefinitionState = { Unloaded: 0, Loading: 1, Active: 2, Error: 3 };
 
         //Construct module definition. Required before boot to identify necessary modules.
-        $magma.ModuleDefinition = function () {
+        $magma.ModuleDefinition = function (name, location, type) {
 
-            //Private Members
+            //Private Members & init.
             var _internalName = "";
             var _location = "";
             var _moduleType = $magma.ModuleDefinitionType.Undefined;
+            var _moduleState = $magma.ModuleDefinitionState.Unloaded;
+            var _workerRef = null;
+            var _lastCall = Date.now();
+            var _loadFunc = function () {
+                if (_moduleState == 0) {
+                    _moduleState = 1;
+
+                    /*
+                    var scriptLoader = document.createElement("script");
+                    scriptLoader.src = _location;
+                    scriptLoader.setAttribute("type", "text/javascript");
+                    document.head.appendChild(scriptLoader);
+                    */
+                    _workerRef = new Worker(_location);
+                    _workerRef.addEventListener('message', function (e) {
+                        var message = e.data;
+                        if (typeof message.StatusCode !== "undefined" && typeof message.VerboseMessage !== "undefined") {
+                            switch (message.Name.toLowerCase()) {
+                                case "_sys": {
+                                    if (typeof(message.Command) !== "undefined" && typeof(message.Args) !== "undefined") {
+                                        switch (message.Command.toLowerCase()) {
+                                            case "alive": {
+                                                //This is a heartbeat to tell us the application is not timing out.
+                                                _lastCall = Date.now();
+                                            } break;
+                                            case "stop": {
+                                                _moduleState = $magma.ModuleDefinitionStatmessage.Unloaded;
+                                                self.close();
+                                            } break;
+                                            default: {
+                                                throw new $magma.Exception("The module '" + _internalName + "' sent an illegal system command '" + message.Command + "'.");
+                                            } break;
+                                        }
+                                    }
+                                } break;
+                                default: {
+                                    //Tranpose the object.
+                                    var tsMessage = new $magma.Message();
+                                    tsMessage.StatusCode = message.StatusCode;
+                                    tsMessage.SenderIdentity = _internalName;
+                                    tsMessage.VerboseMessage = message.VerboseMessage;
+                                    try {
+                                        tsMessage.Name = message.Name;
+                                        tsMessage.Payload = message.Payload;
+                                    } catch (a) {
+
+                                    }
+                                    $magma.Tell(tsMessage);
+                                } break;
+                            }
+                        } else {
+                            throw new $magma.Exception("The module '" + _internalName + "' passed a malformed object. The message was disposed.");
+                        }
+                        //$magma.Tell(e);
+                    })
+                    
+                } else {
+                    throw new $magma.Exception("The module '" + _internalName + "' cannot be loaded because an attempt to load it was already made.");
+                }
+            }
+
+            if (typeof (type) !== "undefined") {
+                _moduleType = type;
+            }
+
+            if (_internalName.length > 20) {
+                throw new $magma.Exception("The length of module name '" + name + "' exceeds 20 characters. Could not create module definition.");
+            }
+
+            if (typeof (name) !== "undefined") {
+                _internalName = name;
+            } else {
+                throw new $magma.Exception("The name of the module must be provided. Could not create module definition.");
+            }
+
+
+            if (typeof (location) !== "undefined") {
+                _location = location;
+            } else {
+                throw new $magma.Exception("The location of the module source *.js must be provided. Could not create module definition.");
+            }
 
             //Public Members
-            Object.defineProperty(this,"Name",{get:function(){return this._internalName;}});
+            Object.defineProperty(this, "Name", { get: function () { return _internalName; } });
+            Object.defineProperty(this, "State", { get: function () { return _moduleState; } });
 
+            this.Load = _loadFunc;
 
-
-            Object.freeze(this);
         };
+
+        Object.freeze($magma.ModuleDefinition);
 
         //Construct a Message object when you want to post a message to a web worker. 
         $magma.Message = function () {
             this.StatusCode = $magma.Status.Undefined;
             this.Name = "";
+            this.SenderIdentity = null;
             this.VerboseMessage = "";
             this.Payload = {};
         }
+
+        //The scheduler command object tells the scheduler what to do with the web worker.
+        $magma.SchedulerCommand = function () {
+            this.Command = "";
+            this.Args = {};
+            this.Reason = "";
+        }
+
+        Object.freeze($magma.SchedulerCommand);
+
+        Object.freeze($magma.Message);
+
+        //Seal the deal. ;)
+        (function () {
+            var _messages = new $magma.MessageBus();
+        }.bind($magma))();
+
+        Object.freeze($magma);
+
+        //GO
+        $magma.Init();
 
     }
 })();
